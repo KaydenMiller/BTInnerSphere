ecd5c5a859b38272b3e9403cc93da0d710cb40ee
Kayden Miller <kaydenmiller1@gmail.com>
2024-04-01 21:41:57 -0600

    chore: update notes

--- KaydenMiller.BattleTech.Helper.Cli/Program.cs
+++ KaydenMiller.BattleTech.Helper.Cli/Program.cs
@@ -92,4 +92,5 @@ File.WriteAllText("solar-systems-2.json", systemJson);
 // Systems are parsed, with 12 errors - Somehow fix issue with missing - between date and faction and now have new errors
 // Systems are parsed, with 9 errors - support ' ' values between years
 // Systems are parsed, with 5 errors - support spaces around % values and 3 digit percent
+// Systems are parsed, with 0 errors - fix remainder of weird errors
 Console.WriteLine("Done!");
